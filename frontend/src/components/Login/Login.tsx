e1e948d9371ce9d2e0f535413db030be79dae6e2
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-11-28 10:05:31 -0300

    feat(member-list): member List to add to an event implemented

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,27 +1,35 @@
-import { useState } from 'react';
-import Select, { MultiValue } from 'react-select';
+// import { useState } from 'react';
+// import Select, { MultiValue } from 'react-select';
 
-const values = [
-  { value: '1', label: 'Gustavo' },
-  { value: '2', label: 'Christa' },
-  { value: '3', label: 'Brooke' },
-  { value: '4', label: 'John' },
-];
+// const values = [
+//   { value: '1', label: 'Gustavo' },
+//   { value: '2', label: 'Christa' },
+//   { value: '3', label: 'Brooke' },
+//   { value: '4', label: 'John' },
+// ];
 
-export const Login = (): JSX.Element => {
-  const [select, setSelect] = useState<MultiValue<{ value: string; label: string }>>([]);
-  console.log(select);
+// export const Login = (): JSX.Element => {
+//   const [select, setSelect] = useState<MultiValue<{ value: string; label: string }>>([]);
+//   console.log(select);
 
+//   return (
+//     <div>
+//       <Select
+//         isMulti
+//         options={values}
+//         isClearable={true}
+//         isSearchable={true}
+//         closeMenuOnSelect={false}
+//         onChange={item => setSelect(item)}
+//       />
+//     </div>
+//   );
+// };
+
+export const Login = () => {
   return (
     <div>
-      <Select
-        isMulti
-        options={values}
-        isClearable={true}
-        isSearchable={true}
-        closeMenuOnSelect={false}
-        onChange={item => setSelect(item)}
-      />
+      <h1>Login</h1>
     </div>
   );
 };
