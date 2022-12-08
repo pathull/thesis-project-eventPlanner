5f91ecdc322a7715b84de522887615c23464378a
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-12-08 12:53:03 -0300

    test(backend-test): formatting for backend tests

---
+++ backend/__tests__/create-event.test.ts
@@ -0,0 +1,67 @@
+import 'dotenv/config';
+import express from 'express';
+import { describe, beforeAll, afterEach, expect, test, afterAll } from '@jest/globals';
+import { faker } from '@faker-js/faker';
+import supertest from 'supertest';
+
+import { UserSchema } from '../src/models/schemas/user-schemas';
+import { EventsSchema } from '../src/models/schemas/event-schema';
+import { sequelize } from '../src/models/connectionDb'
+import appRoutes from '../src/routes/index'
+
+describe('Integration tests for Create Event routes', () => {
+  const app = express();
+  app.use(express.json());
+  const request = supertest(app);
+  app.use('/', appRoutes);
+
+  beforeAll(async () => {
+    await sequelize.sync({ force:true })
+  });
+
+  afterEach(async () => {
+    await UserSchema.destroy({ where:{}, force: true });
+    await EventsSchema.destroy({ where:{}, force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  describe('Post /event', () => {
+    test('Should successfully create an event w/o picture', async () => {
+      const user = await UserSchema.create({ email: faker.internet.email(), username: faker.internet.userName(), picUrl: faker.image.image(), name: faker.internet.userName() });
+
+      const mockEventData = {
+        createdBy: user.getDataValue('id'),
+        location: faker.lorem.sentence(2),
+        eventDate:faker.lorem.sentence(2),
+        eventName:faker.lorem.sentence(2),
+        description: faker.lorem.sentence(10),
+      };
+
+      const res = await request.post(`/api/events`).send(mockEventData);
+      expect(res.statusCode).toBe(201);
+      expect(res.body.location).toMatch(mockEventData.location);
+      expect(res.body.description).toMatch(mockEventData.description);
+    });
+
+    test('Should successfully create an event with a picture', async () => {
+      const user = await UserSchema.create({ email: faker.internet.email(), username: faker.internet.userName(), picUrl: faker.image.image(), name: faker.internet.userName() });
+
+      const response = await request.post('/api/events')
+      .set("Content-Type", "multipart/form-data")
+      .field("createdBy", `${user.getDataValue('id')}`)
+      .field("eventName", "the place")
+      .field("eventDate", "anything" )
+      .field("location", "1111 street")
+      .field("description", "hello there")
+      .attach(
+        "eventPic",
+        "./__tests__/MJ.jpg"
+      )
+
+      expect(response.statusCode).toBe(201);
+    });
+  });
+});
--- backend/__tests__/user.test.ts
+++ backend/__tests__/user.test.ts
@@ -3,48 +3,50 @@ import express from 'express';
 import { describe, beforeAll, afterEach, expect, test, afterAll } from '@jest/globals';
 import { faker } from '@faker-js/faker';
 import supertest from 'supertest';
+
 import { UserSchema } from '../src/models/schemas/user-schemas';
 import { AppErrors } from '../src/helpers/app-error';
 import { sequelize } from '../src/models/connectionDb'
 import appRoutes from '../src/routes/index'
 
-
 describe('Integration tests for Backend User Route', () => {
   const app = express();
   app.use(express.json());
-  const request = supertest(app)
+  const request = supertest(app);
   app.use('/', appRoutes);
+
   beforeAll(async () => {
     await sequelize.sync({ force:true })
   });
 
   afterEach(async () => {
-    await UserSchema.destroy({where:{}, force: true});
+    await UserSchema.destroy({ where:{}, force: true });
   });
 
   afterAll(async () => {
     await sequelize.close();
   });
 
-describe('Post /user', () => {
-  test('Should return user and status 201', async () => {
-    const email = faker.internet.email();
-    const res = await request.post('/api/users').send({ email })
-    expect(res.statusCode).toBe(201)
-    expect(res.headers['content-type']).toEqual(expect.stringContaining('json'));
+  describe('Post /user', () => {
+    test('Should return user and status 201', async () => {
+      const email = faker.internet.email();
+      const res = await request.post('/api/users').send({ email });
+
+      expect(res.statusCode).toBe(201)
+      expect(res.headers['content-type']).toEqual(expect.stringContaining('json'));
       expect(res.body.id).toBeDefined();
       expect(res.body.email).toBe(email);
-  });
+    });
 
-  test('Should return status code 400 and throw an error when email is invalid', async () => {
-    const email = 'invalid'
-    try {
-      await request.post('/api/users').send({ email });
-    } catch (err) {
-      expect(err).toBeInstanceOf(AppErrors);
-      expect((err as AppErrors).status).toBe(400);
+    test('Should return status code 400 and throw an error when email is invalid', async () => {
+      const email = 'invalid';
+
+      try {
+        await request.post('/api/users').send({ email });
+      } catch (err) {
+        expect(err).toBeInstanceOf(AppErrors);
+        expect((err as AppErrors).status).toBe(400);
       }
     });
   });
 });
-
