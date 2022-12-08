e999fa08793dac283f67194f7cd055ce826a93f3
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-12-08 16:54:18 -0300

    refactor(calendar): calendar componenet was refactor and optimized

--- frontend/src/components/CalendarProfile/CalendarProfile.tsx
+++ frontend/src/components/CalendarProfile/CalendarProfile.tsx
@@ -1,45 +1,37 @@
 import { useState, useEffect } from 'react';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import enUS from 'date-fns/locale/en-US';
-import addMinutes from 'date-fns/addMinutes';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { retrieveAllListOfEvents } from '../../services/fetch-events';
+import { errorToast } from '../../helpers/toasts';
+import { defaultEndDate } from '../../helpers/app-functions';
 import { ICalendarEvent } from '../../types/app-types';
 
-const locales = {
-  'en-US': enUS,
-};
-
-const endOfHour = (date: Date): Date => addMinutes(date, 60);
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+const localizer = momentLocalizer(moment);
 
 export const CalendarProfile = (): JSX.Element => {
   const [events, setEvents] = useState<ICalendarEvent[]>([]);
 
   const getEvents = async () => {
     const response = await retrieveAllListOfEvents();
-    const updateProp = response?.map(event => {
-      return {
-        title: event.eventName,
-        start: new Date(event.eventDate),
-        end: addMinutes(endOfHour(new Date(event.eventDate)), 120),
-      };
-    });
-    if (updateProp) {
+
+    if (response) {
+      if ('error' in response) {
+        errorToast('Error Uploading user events, Our team will be fixing this!');
+        return;
+      }
+
+      const updateProp = response.map(event => {
+        return {
+          title: event.eventName,
+          start: new Date(event.eventDate),
+          end: defaultEndDate(event.eventDate),
+        };
+      });
+
       setEvents(updateProp);
     }
   };
